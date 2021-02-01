a76096704a8b10a4c42bdd065a5c3bea86cab80a
Gamer1000YT <68267998+Gamer1000YT@users.noreply.github.com>
2021-02-01 21:37:40 +0200

    Add files via upload

---
+++ sites/Username.py
@@ -0,0 +1,14 @@
+username= input("Enter Username")
+print(Username is: +username)
+
+html_file =open("Normal.html" ','w')
+a = [ 'f' , 'd' , 's' , 'a']
+x = -1
+scope = vars()
+data = ''
+for i in a #TIP: use a generator
+scope [ 'x' ]+=1
+data += a[x]
+data += '\n'
+html_fie.write(data)
+html_file.close()
